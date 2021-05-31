4d7946ee077d77a84ad336e044d9ff41c3ba80a6
Zaeem Tarrar <zaeemtarrar3@gmail.com>
2021-05-31 11:53:53 +0500

    javascript 0.0.3

--- document-object-model/index.js
+++ document-object-model/index.js
@@ -1,39 +1,5 @@
-
-const Step1 = async () => {
-    let Url = 'https://jsonplaceholder.typicode.com/todos/1';
-    let response = await fetch(Url)
-        .then(response => response.json());
-    return response;
-};
-
-const Step2 = async () => {
-    let Url = 'https://jsonplaceholder.typicode.com/todos/2';
-    let response = await fetch(Url)
-        .then(response => response.json());
-    return response;
-};
-
-const Step3 = async () => {
-    let Url = 'https://jsonplaceholder.typicode.com/todos/3';
-    let response = await fetch(Url)
-        .then(response => response.json());
-    return response;
-};
-
 const Launch = () => {
-    let Chain = new Promise((resolve, reject) => resolve() );
-    
-    Chain.then( () => {
-        return Step1();
-    }).then((A)=>{
-        return [A, Step2()];
-    }).then(([A,B])=>{
-        return [A, B, Step3()];
-    }).then(([A,B,C])=>{
-        console.log(A,B,C);
-    }).catch(err => {
-        console.error(err);
-    })
+    console.log("Window Ready");
 }
 
 window.onload = Launch
